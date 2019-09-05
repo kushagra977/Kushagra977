6be62ba23ca79b7f1856055a8a251ba5b01dda87
kushagra977 <kushagragautam0706@gmail.com>
2019-09-05 18:12:59 +0530

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }    from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { TShirtDetailsComponent } from './tshirt-details/tshirt-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TShirtDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tshirt-details/tshirt-details.component.ts
@@ -0,0 +1,38 @@
+import {EventEmitter, Component, OnInit, Input, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tshirt-details',
+  templateUrl: './tshirt-details.component.html',
+  styleUrls: ['./tshirt-details.component.css']
+})
+export class TShirtDetailsComponent implements OnInit {
+
+  @Input() tshirt;
+  quantity;
+  error=false;
+  @Output() cancelevent=new EventEmitter();
+  @Output() buyevent=new EventEmitter();
+  ngOnChanges(): void{
+
+  }
+  buy(){
+    if(this.quantity){
+      if(this.quantity>this.tshirt.inStock){
+        alert("not available");
+      }
+      else{
+        this.tshirt.inStock=this.tshirt.inStock-this.quantity;
+        this.buyevent.emit(this.tshirt);
+      }
+    }
+  }
+  cancel(){
+    this.cancelevent.emit(false);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
